b4dd7f0c05cc16581e62a8f0df65f7a2a27f65e2
Your Name <jefersonrl@icloud.com>
2019-04-18 09:45:01 -0300

    Aula de hoje

---
+++ Atividade1/src/atv/Execucao.java
@@ -0,0 +1,17 @@
+package atv;
+
+import atv.Pessoa;
+
+public class Execucao {
+
+	public static void main(String[] args) {
+		
+		Pessoa p = new Pessoa();
+		String x = null, y = null;
+		
+		System.out.println(p.andar(x));
+		System.out.println(p.respirar(y));
+		
+	}
+	
+}
---
+++ Atividade1/src/atv/Pessoa.java
@@ -0,0 +1,22 @@
+package atv;
+
+public class Pessoa {
+	
+	public String andar(String x) {
+		 x = "Andando";
+		return x;
+	}
+	
+	protected String respirar(String y) {
+		 y = "Respirando";
+		return y;
+	}
+	
+	private String dormir(String z) {
+		 z = "Dormindo";
+		return z;
+	}
+
+	
+
+}
